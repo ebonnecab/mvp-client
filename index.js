dab3695e9147428eebba13a3f4d89a3e2c0d6f1c
t0ri <victoriarosemurray@gmail.com>
2019-02-06 11:24:20 -0800

    added js

---
+++ index.js
@@ -0,0 +1,7 @@
+const loginUsername = document.getElementById('login-username').value
+const loginPassword = document.getElementById('login-password').value
+const loginBtn = document.getElementById('login-submit')
+
+loginBtn.addEventListener('click', (e) => {
+  console.log('ok')
+});
