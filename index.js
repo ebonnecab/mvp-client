f9e2828e3c0b2b4b1f1037a1e13108b594ed691b
Connor Cahill <connorcahill913@gmail.com>
2019-02-06 13:52:22 -0800

    [Added]: started Javascript logic for making api calls to backend. [Next]: finish index.html by putting in data from api call.

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 const loginUsername = document.getElementById('login-username')
 const loginPassword = document.getElementById('login-password')
 const loginBtn = document.getElementById('login-submit')
+/******
+ * NOTE: we need to actaully send the backend live if we want this to work
+ * Have to make api call to live backend 
+ * 
+ ******/
 
-loginBtn.addEventListener('click', (e) => {
-  console.log(loginUsername.value + loginPassword.value)
-});
+async function signUserIn() {
+  form = document.getElementById('auth-form');
+  const data = new FormData(form);
+  const res = await fetch('/api/auth/sign-in', {
+    method: 'post',
+    body: data,
+  });
+  console.log(res.j);
+}
+
+
+/***********
+ * 
+ * Below is the frontend logic for the INDEX Page.
+ * The following functions will...
+ * 1. make API Call 
+ * 2. Return data and append it into html
+ * 3. 
+ */
+
+ async function getArt() {
+   // Url for backend
+   const url = 'https://squilla-api.herokuapp.com';
+  
+    //  fetching the art from the API
+   const artRes = await fetch(`${url}/api/art`)
+   const art = await artRes.json();
+    /// what is this reponse??
+   console.log(art);
+
+
+ }
+
+ /****
+  * 
+  * NEXT:
+  * need to fix CORS with backend so request will go through.
+  * Then do the To-dos listed above
+  */
+
+ getArt();
